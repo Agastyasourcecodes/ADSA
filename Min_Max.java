78498be47ae863ec3e3bf44151e781b4f4bcd39b
Agastyasourcecodes <139317735+Agastyasourcecodes@users.noreply.github.com>
2025-08-31 09:35:24 +0530

    Create Min_Max.java

---
+++ Min_Max.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+            int[] a = new int[n];
+            int min = Integer.MAX_VALUE;
+            for (int i = 0; i < n; i++) {
+                a[i] = scanner.nextInt();
+                if (a[i] < min) min = a[i];
+            }
+            int ops = 0;
+            for (int val : a) if (val > min) ops++;
+            System.out.println(ops);
+        }
+    }
+}
